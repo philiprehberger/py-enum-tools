2c843d36c38398ef4c372f0ce6ac2b254c004e59
Philip Rehberger <me@philiprehberger.com>
2021-09-26 16:09:25 -0500

    Release v0.1.5

---
+++ tests/test_import.py
@@ -0,0 +1,7 @@
+"""Basic import test."""
+
+
+def test_import():
+    """Verify the package can be imported."""
+    import philiprehberger_enum_tools
+    assert hasattr(philiprehberger_enum_tools, "__name__") or True
